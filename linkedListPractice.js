f2ccbfbb4bdeb239b14db8e4c8bb96d27c4a87cc
KevinLaird <kevinlaird16@gmail.com>
2022-10-22 21:17:24 -0700

    Singly Linked List

--- linkedListPractice.js
+++ linkedListPractice.js
@@ -1,38 +1,93 @@
-// let myLinkedList = {
-//   head: {
-//     value: 26,
-//     next: {
-//       value: 5,
-//       next: {
-//         value: 16,
-//         next: null,
-//       },
-//     },
-//   },
-// };
+class Node {
+  constructor(value, next = null) {
+    (this.value = value), (this.next = next);
+  }
+}
 
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
+
   append(value) {
-    const newNode = {
-      value,
-      next: null,
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
+
+  prepend(value) {
+    const newNode = new Node(value, this.head);
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  traverseToIndex(index) {
+    if (index > this.length) return 'Could not find the Node!';
+    let currentNode = this.head;
+    for (let i = 0; i <= index; i++) {
+      if (i === index) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    }
+    if (index >= this.length) {
+      this.append(value);
+      return this;
+    }
+
+    const selectedNode = this.traverseToIndex(index - 1);
+    if (!selectedNode) return 'Something went wrong!';
+
+    const newNode = new Node(value, selectedNode.next);
+    selectedNode.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  remove(index) {
+    const selectedNode = this.traverseToIndex(index - 1);
+    const nodeToDelete = selectedNode.next;
+    selectedNode.next = nodeToDelete.next;
+    this.length--;
+  }
 }
 
-const myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
+// 1 --> 2 --> 3
+// myLinkedList.insert(1, 4)
+// 1 --> 4 --> 2 --> 3
+// curr = this.head (1), index 0
+// if (curr.next === )
+// curr = curr.next (2), index 1
+// const newNode = new Node(value, curr)
+// 1 --> 2
 
-console.log(myLinkedList);
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(11);
+myLinkedList.append(12);
+myLinkedList.append(13);
+myLinkedList.remove(2);
+
+console.log(myLinkedList.printList());
